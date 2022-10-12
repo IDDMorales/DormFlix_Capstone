22a25e937120e895f8e1aa62358561b5b7b2169b
ArciagaGC <gc.arciaga0382@student.tsu.edu.ph>
2022-10-13 00:24:26 +0800

    firebase

--- app/src/main/java/com/example/dormflixv2/Login.java
+++ app/src/main/java/com/example/dormflixv2/Login.java
@@ -1,26 +1,67 @@
 package com.example.dormflixv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    EditText mPassword,mEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mPassword = findViewById(R.id.password);
+        mEmail = findViewById(R.id.emailAdd);
+        mAuth = FirebaseAuth.getInstance();
+
         Button btnLog = findViewById(R.id.btnLog);
         btnLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startMainHome();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+                if(password.length() <8 ){
+                    mPassword.setError("Password Must be >= 8 characters");
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Login Successfully !", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                        }else{
+                            Toast.makeText(Login.this, "Error !" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
 
@@ -28,7 +69,7 @@ public class Login extends AppCompatActivity {
         tv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sMainHome();
+                startActivity(new Intent(getApplicationContext(),SignUp.class));
             }
         });
         ImageButton ibLog = findViewById(R.id.ibLog);
--- app/src/main/java/com/example/dormflixv2/SignUp.java
+++ app/src/main/java/com/example/dormflixv2/SignUp.java
@@ -1,22 +1,27 @@
 package com.example.dormflixv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 
 public class SignUp extends AppCompatActivity {
     private FirebaseAuth mAuth;
-
-
+    EditText mFullName,mEmail,mPassword,mPhone, mConPass;
 
 
     @Override
@@ -24,31 +29,60 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-      EditText fullname = findViewById(R.id.editFName);
-      EditText email = findViewById(R.id.editEMail);
-      EditText phone = findViewById(R.id.editPNum);
-      EditText password = findViewById(R.id.editPass);
-      EditText conPassword = findViewById(R.id.editConPass) ;
-      mAuth = FirebaseAuth.getInstance();
-
-
-
+        mFullName = findViewById(R.id.editFName);
+        mPhone = findViewById(R.id.editPNum);
+        mEmail = findViewById(R.id.editEMail);
+        mPassword = findViewById(R.id.editPass);
+        mConPass = findViewById(R.id.editConPass);
+        mAuth = FirebaseAuth.getInstance();
+        TextView tv = findViewById(R.id.txtLogin);
         Button btnSign = findViewById(R.id.signBtn);
+
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
         btnSign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+                if(password.length() <8 ){
+                    mPassword.setError("Password Must be >= 8 characters");
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(SignUp.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
-
+                        }else{
+                            Toast.makeText(SignUp.this, "Error !" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
 
 
-        TextView tv = findViewById(R.id.txtLogin);
         tv.setOnClickListener(new View.OnClickListener(){
             @Override
-        public void onClick(View v) {sLogin();}
+        public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
         });
         ImageButton ibLog = findViewById(R.id.ibSign);
         ibLog.setOnClickListener(new View.OnClickListener() {
