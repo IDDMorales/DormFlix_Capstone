a04c37af5c8eb648330b353caffa832edc29141e
ArciagaGC <gc.arciaga0382@student.tsu.edu.ph>
2022-10-13 23:29:54 +0800

    Forgot password

--- app/src/main/java/com/example/dormflixv2/Login.java
+++ app/src/main/java/com/example/dormflixv2/Login.java
@@ -1,8 +1,10 @@
 package com.example.dormflixv2;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +25,8 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
     EditText mPassword,mEmail;
+    TextView forgotPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,7 @@ public class Login extends AppCompatActivity {
         mPassword = findViewById(R.id.password);
         mEmail = findViewById(R.id.emailAdd);
         mAuth = FirebaseAuth.getInstance();
+        forgotPass = findViewById(R.id.forgot);
 
         Button btnLog = findViewById(R.id.btnLog);
         btnLog.setOnClickListener(new View.OnClickListener() {
@@ -69,7 +76,7 @@ public class Login extends AppCompatActivity {
         tv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),SignUp.class));
+
             }
         });
 
@@ -84,7 +91,45 @@ public class Login extends AppCompatActivity {
 
 
         /*need one more for forgot password*/
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText reset = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Passowrd ?");
+                passwordResetDialog.setMessage("Enter Your Email To Received Reset Link");
+                passwordResetDialog.setView(reset);
 
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        String email = reset.getText().toString();
+                        mAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(Login.this, "Reset Link Sent To Your Email", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this, "Error! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+            }
+        });{
+
+        }
     }
 
     private void closeLog() {
--- app/src/main/java/com/example/dormflixv2/SignUp.java
+++ app/src/main/java/com/example/dormflixv2/SignUp.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,14 +15,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.core.Tag;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class SignUp extends AppCompatActivity {
+    public static final String TAG = "TAG";
     private FirebaseAuth mAuth;
     EditText mFullName,mEmail,mPassword,mPhone, mConPass;
+    String userID;
 
 
     @Override
@@ -35,6 +46,7 @@ public class SignUp extends AppCompatActivity {
         mPassword = findViewById(R.id.editPass);
         mConPass = findViewById(R.id.editConPass);
         mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         TextView tv = findViewById(R.id.txtLogin);
         Button btnSign = findViewById(R.id.signBtn);
 
@@ -46,6 +58,9 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String fullname = mFullName.getText().toString().trim();
+                String phone = mPhone.getText().toString().trim();
+                String Confirmpass = mConPass.getText().toString().trim();
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is Required");
@@ -63,8 +78,22 @@ public class SignUp extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
+
                             Toast.makeText(SignUp.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = mAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = db.collection("Users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fname",fullname);
+                            user.put("email", email);
+                            user.put("phone", phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG,"OnSuccess: user Profile is created for "+ userID);
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(), Login.class));
+
 
                         }else{
                             Toast.makeText(SignUp.this, "Error !" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
@@ -79,7 +108,7 @@ public class SignUp extends AppCompatActivity {
         tv.setOnClickListener(new View.OnClickListener(){
             @Override
         public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Login.class));
+
             }
         });
         ImageButton ibLog = findViewById(R.id.ibSign);
