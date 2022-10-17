7d553f4faafae9bc9e0fe7f415aa58b88e5e3035
IDDMorales <111698234+IDDMorales@users.noreply.github.com>
2022-10-17 21:01:24 +0800

    Payment and Feedback

---
+++ app/src/main/java/com/example/dormflixv2/feedbackinitial.java
@@ -0,0 +1,6 @@
+package com.example.dormflixv2;
+
+import android.app.Activity;
+
+public class feedbackinitial extends Activity {
+}
---
+++ app/src/main/java/com/example/dormflixv2/paypal3.java
@@ -0,0 +1,6 @@
+package com.example.dormflixv2;
+
+import android.app.Activity;
+
+public class paypal3 extends Activity {
+}
