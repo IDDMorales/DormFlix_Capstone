1ed62176977aa8813dc6a0fdf78dbd15806b94b3
ArciagaGC <gc.arciaga0382@student.tsu.edu.ph>
2022-10-26 23:23:04 +0800

    verify email

--- app/src/main/java/com/example/dormflixv2/verify.java
+++ app/src/main/java/com/example/dormflixv2/verify.java
@@ -7,17 +7,21 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -29,8 +33,6 @@ import java.io.File;
 
 public class verify extends AppCompatActivity {
     EditText edit;
-    StorageReference storageReference;
-    DatabaseReference df;
     Button upload;
 
 
@@ -38,96 +40,34 @@ public class verify extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
+
         edit = findViewById(R.id.fileupload);
         upload = findViewById(R.id.btnUp);
 
-        storageReference = FirebaseStorage.getInstance().getReference();
-        df = FirebaseDatabase.getInstance().getReference("pdfs");
-        upload.setEnabled(false);
-        edit.setOnClickListener(new View.OnClickListener() {
+        upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                selectPDF();
+                validateEmail();
             }
         });
 
 
     }
 
-    private void selectPDF() {
-        Intent intent = new Intent();
-        intent.setType("application/pdf");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent,"Select PDF files"),101);
-    }
-
-    @SuppressLint("Range")
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 101 && resultCode == RESULT_OK && data != null && data.getData() !=null){
-            Uri uri =data.getData();
-
-
-            String uriString = uri.toString();
-            File myFile = new File(uriString);
-            String path = myFile.getAbsolutePath();
-            String displayName= null;
-
-            if(uriString.startsWith("content://")) {
-                Cursor cursor = null;
-                try {
-                    cursor = this.getContentResolver().query(uri, null, null, null, null);
-                    if (cursor != null && cursor.moveToFirst()) {
-                        displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                    }
-
-                } finally {
-                    cursor.close();
-                }  if (uriString.startsWith("file://")) {
-                    displayName = myFile.getName();
-                }
-                upload.setEnabled(true);
-                edit.setText(displayName);
-
-                upload.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        uploadPDF(data.getData());
-                    }
-                });
-            }
+    private void validateEmail() {
+        String email = edit.getText().toString().trim();
+        if(TextUtils.isEmpty(email)){
+            edit.setError("Email is required");
+            return;
         }
-    }
-
-    private void uploadPDF(Uri data) {
-        final ProgressDialog  pd = new ProgressDialog(this);
-        pd.setTitle("File Uploading..");
-        pd.show();
-
-        final StorageReference reference = storageReference.child("uploads/"+ System.currentTimeMillis() + ".pdf");
-        // store in upload folder of the Firebase storage
-        reference.putFile(data)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
-                        while (!uriTask.isComplete());
-                        Uri uri = uriTask.getResult();
-
-                        File file = new File(edit.getText().toString(), uri.toString()); //get the views from the model class
-                        df.child(df.push().getKey()).setValue(file);// push the value into the realtime database
-                        Toast.makeText(verify.this, "File Uploaded Successfully!!", Toast.LENGTH_SHORT).show();
-                        pd.dismiss();
-
-                    }
-                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-                        float percent = (100 * snapshot.getBytesTransferred())/ snapshot.getTotalByteCount();
-                        pd.setMessage("Uploaded : "+ (int) percent + "%");
-                    }
-                });
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user.isEmailVerified()){
+            startActivity(new Intent(verify.this, mainHome.class));
+        }else{
+            user.sendEmailVerification();
+            Toast.makeText(verify.this, "Check your email to verify", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(getApplication(), mainHome.class));
+        }
 
     }
 }
--- app/src/main/java/com/example/dormflixv2/verifyemail.java
+++ app/src/main/java/com/example/dormflixv2/verifyemail.java
@@ -1,6 +1,13 @@
 package com.example.dormflixv2;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class verifyemail extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifyemail);
+
+    }
 }
